7e0877217d1c7e47df9213948d886f7ca1960ea1
JelenaTomic <tomic.elena09@gmail.com>
2022-11-09 01:22:40 +0100

    added reesolvers in schema

---
+++ server/schema/resolvers.js
@@ -0,0 +1,21 @@
+const { User } = require('../models');
+
+const resolvers = {
+    Query: {
+        user: async () => {
+            return User.findOne({ _id: userId })
+        }
+    },
+
+    Mutation: {
+        addUser: async (parent, { body }) => {
+            return User.create({ body });
+        },
+        signToken: async (parent, { userId, token }) => {
+            return User.findOneAndUpdate(
+                { _id: userId },
+                { new: true, runValidators: true }
+            )
+        }
+    }
+}
